6dbda999095db382d1d5a5b13b785b1e3e7c99dc
Youssef Ahmed Nashaat El-Arian <112262361+youssefnashaatt@users.noreply.github.com>
2024-04-18 12:19:53 +0200

    Add files via upload

---
+++ src/Unit_Testing/fileValidatorTest.java
@@ -0,0 +1,50 @@
+package Unit_Testing;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import Implementation.fileValidator;
+
+public class fileValidatorTest {
+
+    @Test
+    public void testValidSubjectLine() {
+        fileValidator validator = new fileValidator();
+        assertTrue(validator.validateLine("Mathematics,CSE215s,100", 0));
+    }
+
+    @Test
+    public void testInvalidSubjectLine() {
+        fileValidator validator = new fileValidator();
+        assertFalse(validator.validateLine("Mathematics,100", 0));
+        assertFalse(validator.validateLine("Mathematics,CSE215s,100,Extra", 0));
+    }
+
+    @Test
+    public void testValidStudentLine() {
+        fileValidator validator = new fileValidator();
+        assertTrue(validator.validateLine("Youssef Ahmed,1900124,8,9,18,45", 1));
+    }
+
+    @Test
+    public void testInvalidStudentLine() {
+        fileValidator validator = new fileValidator();
+        assertFalse(validator.validateLine("Youssef,8,9,18", 1));
+        assertFalse(validator.validateLine("Youssef Ahmed,1900124,8,9,18,45,Extra", 1));
+    }
+    
+    @Test
+    public void testValidStudent_LineNumber2()
+    {
+        fileValidator validator = new fileValidator();
+        assertTrue(validator.validateLine("Youssef Ahmed,1900124,8,9,18,45", 2));
+    }
+    
+    @Test
+    public void testValidStudent_LineNumber1000()
+    {
+        fileValidator validator = new fileValidator();
+        assertTrue(validator.validateLine("Youssef Ahmed,1900124,8,9,18,45", 1000));
+    }
+}
