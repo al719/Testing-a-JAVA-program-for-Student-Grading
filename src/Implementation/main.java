fee1913a2a1055f489722ce97a483377cfdddcca
al719 <alaam62021@gmail.com>
2024-05-12 22:56:53 +0300

    update main

--- src/Implementation/main.java
+++ src/Implementation/main.java
@@ -1,13 +1,15 @@
-package Implementation;
+package testing_prj;
 
 import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class main {
 
 	public static void main(String[] args) throws FileNotFoundException {
 		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
 		
-		FileHandler filehandle = new FileHandler("..\\test_sample.txt");
+		FileHandler filehandle = new FileHandler(s.nextLine());
 //		filehandle.setPath("F:\\senior\\2nd\\Testing\\project\\phase1\\testing_prj\\src\\testing_prj\\Fa_BBTC10.txt");//F:\\senior\\2nd\\Testing\\project\\phase1\\testing_prj\\src\\testing_prj\\
 		outputGenerator out = new outputGenerator(filehandle);
 		out.generateOutputFile();
