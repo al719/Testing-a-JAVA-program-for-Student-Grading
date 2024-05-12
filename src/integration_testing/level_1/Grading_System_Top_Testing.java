b222ca65332a6d67581b7d45b475cd1329b97be1
al719 <alaam62021@gmail.com>
2024-05-12 22:53:32 +0300

    add 18 test cases for top module

--- src/integration_testing/level_1/Grading_System_Top_Testing.java
+++ src/integration_testing/level_1/Grading_System_Top_Testing.java
@@ -145,6 +145,332 @@ class Grading_System_Top_Testing {
 	}
 	
 	
+	
+	@Test
+	void test3() throws Exception {
+		String userInput = String.format("F:\\senior\\2nd\\Testing\\project\\phase1\\phase2\\test cases\\Fa_BBTC03.txt");
+        ByteArrayInputStream bais = new ByteArrayInputStream(userInput.getBytes());
+
+        System.setIn(bais);
+        
+       
+		main.main(null);
+		File myfile = new File("computer_netw!rking.txt");
+		///////////////////////////////////////////////////////////////////////////////////
+		/////////////////// check output file is correctly not generated    ///////////////////
+		///////////////////////////////////////////////////////////////////////////////////
+		assertFalse(myfile.exists());
+		
+		
+		
+	}
+	
+	@Test
+	void test4() throws Exception {
+		String userInput = String.format("F:\\senior\\2nd\\Testing\\project\\phase1\\phase2\\test cases\\Fa_BBTC04.txt");
+        ByteArrayInputStream bais = new ByteArrayInputStream(userInput.getBytes());
+
+        System.setIn(bais);
+        
+       
+		main.main(null);
+		File myfile = new File("computer_netw!rking.txt");
+		///////////////////////////////////////////////////////////////////////////////////
+		/////////////////// check output file is correctly not generated    ///////////////////
+		///////////////////////////////////////////////////////////////////////////////////
+		assertFalse(myfile.exists());
+		
+		
+		
+	}
+	@Test
+	void test5() throws Exception {
+		String userInput = String.format("F:\\senior\\2nd\\Testing\\project\\phase1\\phase2\\test cases\\Fa_BBTC05.txt");
+        ByteArrayInputStream bais = new ByteArrayInputStream(userInput.getBytes());
+
+        System.setIn(bais);
+        
+       
+		main.main(null);
+		File myfile = new File("computer_netw!rking.txt");
+		///////////////////////////////////////////////////////////////////////////////////
+		/////////////////// check output file is correctly not generated    ///////////////////
+		///////////////////////////////////////////////////////////////////////////////////
+		assertFalse(myfile.exists());
+		
+		
+		
+	}
+	@Test
+	void test6() throws Exception {
+		String userInput = String.format("F:\\senior\\2nd\\Testing\\project\\phase1\\phase2\\test cases\\Fa_BBTC06.txt");
+        ByteArrayInputStream bais = new ByteArrayInputStream(userInput.getBytes());
+
+        System.setIn(bais);
+        
+       
+		main.main(null);
+		File myfile = new File("computer_netw!rking.txt");
+		///////////////////////////////////////////////////////////////////////////////////
+		/////////////////// check output file is correctly not generated    ///////////////////
+		///////////////////////////////////////////////////////////////////////////////////
+		assertFalse(myfile.exists());
+		
+		
+		
+	}
+	@Test
+	void test7() throws Exception {
+		String userInput = String.format("F:\\senior\\2nd\\Testing\\project\\phase1\\phase2\\test cases\\Fa_BBTC07.txt");
+        ByteArrayInputStream bais = new ByteArrayInputStream(userInput.getBytes());
+
+        System.setIn(bais);
+        
+       
+		main.main(null);
+		File myfile = new File("computer_netw!rking.txt");
+		///////////////////////////////////////////////////////////////////////////////////
+		/////////////////// check output file is correctly not generated    ///////////////////
+		///////////////////////////////////////////////////////////////////////////////////
+		assertFalse(myfile.exists());
+		
+		
+		
+	}
+	@Test
+	void test8() throws Exception {
+		String userInput = String.format("F:\\senior\\2nd\\Testing\\project\\phase1\\phase2\\test cases\\Fa_BBTC08.txt");
+        ByteArrayInputStream bais = new ByteArrayInputStream(userInput.getBytes());
+
+        System.setIn(bais);
+        
+       
+		main.main(null);
+		File myfile = new File("computer_netw!rking.txt");
+		///////////////////////////////////////////////////////////////////////////////////
+		/////////////////// check output file is correctly not generated    ///////////////////
+		///////////////////////////////////////////////////////////////////////////////////
+		assertFalse(myfile.exists());
+		
+		
+		
+	}
+	@Test
+	void test9() throws Exception {
+		String userInput = String.format("F:\\senior\\2nd\\Testing\\project\\phase1\\phase2\\test cases\\Fa_BBTC09.txt");
+        ByteArrayInputStream bais = new ByteArrayInputStream(userInput.getBytes());
+
+        System.setIn(bais);
+        
+       
+		main.main(null);
+		File myfile = new File("computer_netw!rking.txt");
+		///////////////////////////////////////////////////////////////////////////////////
+		/////////////////// check output file is correctly not generated    ///////////////////
+		///////////////////////////////////////////////////////////////////////////////////
+		assertFalse(myfile.exists());
+		
+		
+		
+	}
+	@Test
+	void test10() throws Exception {
+		String userInput = String.format("F:\\senior\\2nd\\Testing\\project\\phase1\\phase2\\test cases\\Fa_BBTC10.txt");
+        ByteArrayInputStream bais = new ByteArrayInputStream(userInput.getBytes());
+
+        System.setIn(bais);
+        
+       
+		main.main(null);
+		File myfile = new File("computer_netw!rking.txt");
+		///////////////////////////////////////////////////////////////////////////////////
+		/////////////////// check output file is correctly not generated    ///////////////////
+		///////////////////////////////////////////////////////////////////////////////////
+		assertFalse(myfile.exists());
+		
+		
+		
+	}
+	@Test
+	void test11() throws Exception {
+		String userInput = String.format("F:\\senior\\2nd\\Testing\\project\\phase1\\phase2\\test cases\\Fa_BBTC11.txt");
+        ByteArrayInputStream bais = new ByteArrayInputStream(userInput.getBytes());
+
+        System.setIn(bais);
+        
+       
+		main.main(null);
+		File myfile = new File("computer_netw!rking.txt");
+		///////////////////////////////////////////////////////////////////////////////////
+		/////////////////// check output file is correctly not generated    ///////////////////
+		///////////////////////////////////////////////////////////////////////////////////
+		assertFalse(myfile.exists());
+		
+		
+		
+	}
+	@Test
+	void test12() throws Exception {
+		String userInput = String.format("F:\\senior\\2nd\\Testing\\project\\phase1\\phase2\\test cases\\Fa_BBTC12.txt");
+        ByteArrayInputStream bais = new ByteArrayInputStream(userInput.getBytes());
+
+        System.setIn(bais);
+        
+       
+		main.main(null);
+		File myfile = new File("computer_netw!rking.txt");
+		///////////////////////////////////////////////////////////////////////////////////
+		/////////////////// check output file is correctly not generated    ///////////////////
+		///////////////////////////////////////////////////////////////////////////////////
+		assertFalse(myfile.exists());
+		
+		
+		
+	}
+	@Test
+	void test13() throws Exception {
+		String userInput = String.format("F:\\senior\\2nd\\Testing\\project\\phase1\\phase2\\test cases\\Fa_BBTC13.txt");
+        ByteArrayInputStream bais = new ByteArrayInputStream(userInput.getBytes());
+
+        System.setIn(bais);
+        
+       
+		main.main(null);
+		File myfile = new File("computer_netw!rking.txt");
+		///////////////////////////////////////////////////////////////////////////////////
+		/////////////////// check output file is correctly not generated    ///////////////////
+		///////////////////////////////////////////////////////////////////////////////////
+		assertFalse(myfile.exists());
+		
+		
+		
+	}
+	@Test
+	void test14() throws Exception {
+		String userInput = String.format("F:\\senior\\2nd\\Testing\\project\\phase1\\phase2\\test cases\\Fa_BBTC14.txt");
+        ByteArrayInputStream bais = new ByteArrayInputStream(userInput.getBytes());
+
+        System.setIn(bais);
+        
+       
+		main.main(null);
+		File myfile = new File("computer_netw!rking.txt");
+		///////////////////////////////////////////////////////////////////////////////////
+		/////////////////// check output file is correctly not generated    ///////////////////
+		///////////////////////////////////////////////////////////////////////////////////
+		assertFalse(myfile.exists());
+		
+		
+		
+	}
+	@Test
+	void test15() throws Exception {
+		String userInput = String.format("F:\\senior\\2nd\\Testing\\project\\phase1\\phase2\\test cases\\Fa_BBTC15.txt");
+        ByteArrayInputStream bais = new ByteArrayInputStream(userInput.getBytes());
+
+        System.setIn(bais);
+        
+       
+		main.main(null);
+		File myfile = new File("computer_netw!rking.txt");
+		///////////////////////////////////////////////////////////////////////////////////
+		/////////////////// check output file is correctly not generated    ///////////////////
+		///////////////////////////////////////////////////////////////////////////////////
+		assertFalse(myfile.exists());
+		
+		
+		
+	}
+	@Test
+	void test16() throws Exception {
+		String userInput = String.format("F:\\senior\\2nd\\Testing\\project\\phase1\\phase2\\test cases\\Fa_BBTC16.txt");
+        ByteArrayInputStream bais = new ByteArrayInputStream(userInput.getBytes());
+
+        System.setIn(bais);
+        
+       
+		main.main(null);
+		File myfile = new File("computer_netw!rking.txt");
+		///////////////////////////////////////////////////////////////////////////////////
+		/////////////////// check output file is correctly not generated    ///////////////////
+		///////////////////////////////////////////////////////////////////////////////////
+		assertFalse(myfile.exists());
+		
+		
+		
+	}
+	@Test
+	void test17() throws Exception {
+		String userInput = String.format("F:\\senior\\2nd\\Testing\\project\\phase1\\phase2\\test cases\\Fa_BBTC17.txt");
+        ByteArrayInputStream bais = new ByteArrayInputStream(userInput.getBytes());
+
+        System.setIn(bais);
+        
+       
+		main.main(null);
+		File myfile = new File("computer_netw!rking.txt");
+		///////////////////////////////////////////////////////////////////////////////////
+		/////////////////// check output file is correctly not generated    ///////////////////
+		///////////////////////////////////////////////////////////////////////////////////
+		assertFalse(myfile.exists());
+		
+		
+		
+	}
+	@Test
+	void test18() throws Exception {
+		String userInput = String.format("F:\\senior\\2nd\\Testing\\project\\phase1\\phase2\\test cases\\Fa_BBTC18.txt");
+        ByteArrayInputStream bais = new ByteArrayInputStream(userInput.getBytes());
+
+        System.setIn(bais);
+        
+       
+		main.main(null);
+		File myfile = new File("computer_netw!rking.txt");
+		///////////////////////////////////////////////////////////////////////////////////
+		/////////////////// check output file is correctly not generated    ///////////////////
+		///////////////////////////////////////////////////////////////////////////////////
+		assertFalse(myfile.exists());
+		
+		
+		
+	}
+	@Test
+	void test19() throws Exception {
+		String userInput = String.format("F:\\senior\\2nd\\Testing\\project\\phase1\\phase2\\test cases\\Fa_BBTC19.txt");
+        ByteArrayInputStream bais = new ByteArrayInputStream(userInput.getBytes());
+
+        System.setIn(bais);
+        
+       
+		main.main(null);
+		File myfile = new File("computer_netw!rking.txt");
+		///////////////////////////////////////////////////////////////////////////////////
+		/////////////////// check output file is correctly not generated    ///////////////////
+		///////////////////////////////////////////////////////////////////////////////////
+		assertFalse(myfile.exists());
+		
+		
+		
+	}
+	@Test
+	void test20() throws Exception {
+		String userInput = String.format("F:\\senior\\2nd\\Testing\\project\\phase1\\phase2\\test cases\\Fa_BBTC20.txt");
+        ByteArrayInputStream bais = new ByteArrayInputStream(userInput.getBytes());
+
+        System.setIn(bais);
+        
+       
+		main.main(null);
+		File myfile = new File("computer_netw!rking.txt");
+		///////////////////////////////////////////////////////////////////////////////////
+		/////////////////// check output file is correctly not generated    ///////////////////
+		///////////////////////////////////////////////////////////////////////////////////
+		assertFalse(myfile.exists());
+		
+		
+		
+	}
 	//////////////////////////////////////////////////
 	public double GPA_Evaluation(String Grade) {
 		// logic for GPA calc.
